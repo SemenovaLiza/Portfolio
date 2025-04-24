fc59af97b6fde2fccac828ab9f5b8f65d2a1111d
Семенова Елизавета <thealeeeaaa23@gmail.com>
2025-04-23 23:01:42 -0400

    version before docker

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const video = document.querySelector(".video-background");
+const swiperText = new Swiper('.swiper', {
+    speed: 1600, mousewheel: {},
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    }
+})
+swiperText.on('slideChange', function() {
+    gsap.to(video, 1.6, { 
+        currentTime: (video.duration / (this.slides.length - 1 )) * this.realindex,
+        ease: Power2.easeOut
+    })
+})
+swiperText.on('slideChangeTransitionStart', function() {
+    video.classList.add('change')
+}).on('slideChangeTransitionEnd', function() {
+    video.classList.remove('change')
+})

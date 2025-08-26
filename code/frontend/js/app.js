02a41fa3cdd2bb3f788adf80730c37cd16c7fd0b
Семенова Елизавета <thealeeeaaa23@gmail.com>
2025-08-26 00:07:00 -0400

    changed gif to mp4

--- code/frontend/js/app.js
+++ code/frontend/js/app.js
@@ -1,4 +1,7 @@
+const openBtn = document.querySelectorAll(".openModal");
+const closeBtn = document.querySelectorAll(".closeModal");
 const video = document.querySelector('.video-background');
+
 const swiperText = new Swiper('.swiper', {
     speed: 1600, mousewheel: {},
     pagination: {
@@ -10,14 +13,31 @@ const swiperText = new Swiper('.swiper', {
         nextEl: '.swiper-button-next'
     }
 });
+
 swiperText.on('slideChange', function() {
     gsap.to(video, 1.6, {
         currentTime: (video.duration / (this.slides.length)) * this.realIndex,
         ease: Power2.easeOut
     })
 });
+
 swiperText.on('slideChangeTransitionStart', function() {
     video.classList.add('change')
 }).on('slideChangeTransitionEnd', function() {
     video.classList.remove('change')
+});
+
+openBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const modalId = btn.getAttribute("data-modal");
+        const modal = document.getElementById(modalId);
+        modal.classList.add("open");
+        swiperText.mousewheel.disable();
+    });
+});
+closeBtn.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        btn.closest(".modal").classList.remove("open");
+        swiperText.mousewheel.enable();
+    });
 });
